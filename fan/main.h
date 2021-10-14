cc3cefa5fd625cf7e9bf9472df5804a4fa953f0c
ANUSHA B K <85734808+Anushakrishnappa@users.noreply.github.com>
2021-10-14 13:37:38 +0530

    Create main.h

---
+++ fan/main.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.h
+ * Author: Anusha
+ *
+ * Created on 11 October, 2021, 6:25 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#define _XTAL_FREQ  2000000
+#define FAN         RC2
+#define FAN_DDR     TRISC2
+
+#define ON    1
+#define OFF   0
+
+#endif	/* MAIN_H */
+
