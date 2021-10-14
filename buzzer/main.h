d55304615d847dd4ab4df31767b1c6bd5dd34462
ANUSHA B K <85734808+Anushakrishnappa@users.noreply.github.com>
2021-10-14 13:33:28 +0530

    Update main.h

--- buzzer/main.h
+++ buzzer/main.h
@@ -1,1 +1,21 @@
+/* 
+ * File:   main.h
+ * Author: Anusha
+ *
+ * Created on 11 October, 2021, 6:15 PM
+ */
+
+
+#ifndef MAIN_H
+#define	MAIN_H
+#define _XTAL_FREQ  2000000
+#define BUZZER         RC1
+#define BUZZER_DDR     TRISC1
+
+#define ON    1
+#define OFF   0
+    
+#endif	/* MAIN_H */
+
+
 
