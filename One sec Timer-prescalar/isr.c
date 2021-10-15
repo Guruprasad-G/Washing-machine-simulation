2b36396513a0dafbc3b0bf7b0c301db8af88d3dc
ANUSHA B K <85734808+Anushakrishnappa@users.noreply.github.com>
2021-10-15 16:37:01 +0530

    Create isr.c

---
+++ One sec Timer-prescalar/isr.c
@@ -0,0 +1,23 @@
+#include <xc.h>
+#include "main.h"
+
+void __interrupt() isr(void)
+{
+    static unsigned int count = 0;
+    
+    if (TMR0IF == 1)
+    {
+        /* TMR0 Register valur + 6 (offset count to get 250 ticks) + 2 Inst Cycle */
+       // TMR0 = TMR0 + 8;
+        TMR0 = 6; 
+        
+        if (++count == 1250)
+        {
+            count = 0;
+            
+            LED1 = !LED1;
+        }
+        
+        TMR0IF = 0;
+    }
+}
